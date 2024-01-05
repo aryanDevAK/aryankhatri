ed9b809bcc071fed598a6c50351f0088d8799d40
AK-Developers <aryankhatri9811@gmail.com>
2024-01-05 21:43:06 +0530

    portfolio

---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export const NavBar = () => {
+  const [about, experience, projects, contact] = ["About", "Experience", "Projects", "Contact"]
+  const name = "aryan khatri";
+
+  function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
+  return (
+    <div>
+      {/* Desktop Navigation */}
+      <nav className='desktop-nav' id='desktop-nav'>
+        <span className='logo'>{name.toUpperCase()}</span>
+        <div>
+          <ul className='nav-links'>
+            <li><a href='/'>{about}</a></li>
+            <li><a href='/'>{experience}</a></li>
+            <li><a href='/'>{projects}</a></li>
+            <li><a href='/'>{contact}</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* Hamburger Navigation */}
+      <nav className='hamburger-nav' id='hamburger-nav'>
+        <div className='logo'>{name.toUpperCase()}</div>
+        <div className='hamburger-menu'>
+          <div className='hamburger-icon' onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className='menu-links'>
+            <li><a href='/'>{about}</a></li>
+            <li><a href='/'>{experience}</a></li>
+            <li><a href='/'>{projects}</a></li>
+            <li><a href='/'>{contact}</a></li>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
